5aa53bc991b7878d8b5b5bbcc1bab41dcfdcdcab
MacaronFR <denisft77@gmail.com>
2023-12-03 12:56:24 +0100

    Fix warning

--- app/src/main/java/fr/imacaron/crypto/MainActivity.kt
+++ app/src/main/java/fr/imacaron/crypto/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
